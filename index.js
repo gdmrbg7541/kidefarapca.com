a4316255421a3a521e82c43f4ddb6558919848a7
Geylani Demirbağ <gdmrbg7541@gmail.com>
2026-05-24 15:43:22 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -246,12 +246,8 @@ document.addEventListener("DOMContentLoaded", () => {
         "🧐 Harf-i Tarîf.pdf",
         "Arapçada Kelime.pdf",
         "⚔️ Kim Daha Hızlı.pdf",
-        "🔬 Çoğul İsimlerin Merfu, Mansub ve Mecrur Halleri.pdf",
-        "İkiki Kırmızı Kartlar.pdf",
-        "İkiki Mavi Kartlar.pdf",
-        "İkiki Mor Kartlar.pdf",
-        "İkiki Yeşil Kartlar.pdf",
-        "Kalıplardan Cümlelere.pdf"
+        "🔬 Çoğul İsimlerin Merfu, Mansub ve Mecrur Halleri.pdf"
+        
         
     ];
 
